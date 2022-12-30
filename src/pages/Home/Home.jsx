6d9bd019716341b5e8024171d922db2e5834c763
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2022-12-30 07:53:01 +0200

    init: add pagination on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,9 @@
+import 'rc-pagination/assets/index.css';
+
 import { useEffect, useState } from 'react';
+import Pagination from 'rc-pagination';
+
+// import ReactPaginate from 'react-paginate';
 
 import css from './Home.module.css';
 
@@ -7,20 +12,32 @@ import { movieApi } from 'services/api';
 import { MoviesList } from 'components';
 
 export const Home = () => {
-  const [page] = useState(1);
+  const [page, setPage] = useState(1);
   const [trendingMovies, setTrendingMovies] = useState([]);
+  const [totalResults, setTotalResults] = useState();
 
   useEffect(() => {
-    movieApi
-      .getTrendingMovies(page)
-      .then(({ results }) => setTrendingMovies(results));
+    movieApi.getTrendingMovies(page).then(({ results, total_results }) => {
+      setTrendingMovies(results);
+      setTotalResults(total_results);
+    });
   }, [page]);
+
+  const handlePageClick = e => setPage(e);
 
   return (
     <main className={css.home}>
       <div className={`container ${css.homeWrapper}`}>
         <MoviesList movies={trendingMovies} link="movies/" />
       </div>
+      <Pagination
+        onChange={handlePageClick}
+        current={page}
+        total={totalResults}
+        pageSize={20}
+        showLessItems
+        showTitle={false}
+      />
     </main>
   );
 };
