7e81e62d7f42a6c9109aed6075a10dac400548cc
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2022-12-31 06:53:32 +0200

    init: init movie meta as component

--- src/components/MovieDetailsMeta/MovieDetailsMeta.jsx
+++ src/components/MovieDetailsMeta/MovieDetailsMeta.jsx
@@ -1,1 +1,33 @@
 import css from './MovieDetailsMeta.module.css';
+import image404 from '../../images/404.jpg';
+
+export const MovieDetailsMeta = ({ movieDetails }) => {
+  const { title, poster_path, vote_average, release_date, overview, genres } =
+    movieDetails;
+
+  const imageSrc = poster_path
+    ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+    : image404;
+
+  return (
+    <>
+      <img src={imageSrc} alt={title} />
+      <ul className={css.movieDetailsMeta}>
+        <li>
+          <h2>
+            {title} | {Number.parseInt(release_date)}
+          </h2>
+          <p>User score: {vote_average}</p>
+        </li>
+        <li>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+        </li>
+        <li>
+          <h3>Genres</h3>
+          <p>{genres > 0 && genres.map(genre => genre.name).join(', ')}</p>
+        </li>
+      </ul>
+    </>
+  );
+};
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useParams, NavLink, Outlet, useLocation } from 'react-router-dom';
 
+import { MovieDetailsMeta } from 'components';
+
 import css from './MovieDetails.module.css';
-import image404 from '../../images/404.jpg';
 
 import { movieApi } from 'services/api';
 
@@ -18,33 +19,10 @@ export const MovieDetails = () => {
     }
   }, [movieId]);
 
-  const { title, poster_path, vote_average, release_date, overview, genres } =
-    movieDetails;
-
-  const imageSrc = poster_path
-    ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-    : image404;
-
   return (
     <div className={css.movieDetailsWrapper}>
       <NavLink to={backLinkHref}>Go Back</NavLink>;
-      <img src={imageSrc} alt={title} />
-      <ul className={css.movieDetailsMeta}>
-        <li>
-          <h2>
-            {title} | {Number.parseInt(release_date)}
-          </h2>
-          <p>User score: {vote_average}</p>
-        </li>
-        <li>
-          <h3>Overview</h3>
-          <p>{overview}</p>
-        </li>
-        <li>
-          <h3>Genres</h3>
-          <p>{genres > 0 && genres.map(genre => genre.name).join(', ')}</p>
-        </li>
-      </ul>
+      <MovieDetailsMeta movieDetails={movieDetails} />
       <div>
         <h3>Additional information</h3>
         <ul>
