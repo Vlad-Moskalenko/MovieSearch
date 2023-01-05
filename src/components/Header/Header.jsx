38846b2bd16bac18444fdcc53236aee06db026ac
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-01-05 12:43:01 +0200

    fix: fix path gor github

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header className={`${css.header} ${css.headerHome}`}>
       <nav className={css.navigation}>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="filmoteka-React/">Home</NavLink>
         <NavLink to="/movies">Movies</NavLink>
       </nav>
     </header>
