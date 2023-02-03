8366445dcb9aebc75ab6676780ffa907b8fdc158
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-02-03 07:35:41 +0200

    feat: feat add styles and animation for 404 error

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,7 +7,13 @@ export const NotFound = ({ className, title }) => (
     {title ? (
       <p className={css.errorText}>{title}</p>
     ) : (
-      <img className={className || css.error} src={img} alt="Error" />
+      <div className={`${css.errorWrapper} ${className || css.error}`}>
+        <div className={css.eyes}>
+          <div className={css.eyesPosition}></div>
+          <div className={css.eyesPosition}></div>
+        </div>
+        <div className={css.textError}></div>
+      </div>
     )}
   </>
 );
